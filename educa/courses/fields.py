3898c6dd8b7b3c59050f36be29e5b8889dd71b5e
Pavel Lubojacky <pluboj9@gmail.com>
2020-10-06 09:53:04 -0700

    Add ordering for Module and Content

--- educa/courses/fields.py
+++ educa/courses/fields.py
@@ -18,7 +18,7 @@ class OrderField(models.PositiveIntegerField):
 				value = last_item.order + 1
 			except ObjectDoesNotExist:
 				value = 0
-			serattr(model_instance, self.attname, value)
+			setattr(model_instance, self.attname, value)
 			return value
 		else:
 			return super().pre_save(model_instance, add)
--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 # Create your models here.
 class Subject(models.Model):
@@ -31,12 +32,23 @@ class Course(models.Model):
 class Module(models.Model):
 	course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
+	order = OrderField(blank=True, for_fields=['course'])
+
+	def __str__(self):
+		return f'{self.order}. {self.title}'
+
+	class Meta:
+		ordering = ['order']
 
 class Content(models.Model):
 	module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
 	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
 	object_id = models.PositiveIntegerField()
 	item = GenericForeignKey('content_type', 'object_id')
+	order = OrderField(blank=True, for_fields=['module'])
+
+	class Meta:
+		ordering = ['order']
 
 # Content models -------------
 class ItemBase(models.Model):
